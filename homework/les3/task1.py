d618577f9354f4360ed4c2e7443bb882be11ba55
AlinaBarinova <popowa.ai4@gmail.com>
2020-11-05 14:45:13 +0300

    les3 task 2

---
+++ homework/les3/task1.py
@@ -0,0 +1,31 @@
+def asking(text: str, check=False) -> int:
+
+    while True:
+        user_input = input(text)
+        try:
+            user_number = int(user_input)
+        except ValueError as error:
+            if user_input.lower() == 'q':
+                return
+            print('Введено неверное значение')
+            continue
+        if check and user_number == 0:
+            print('Введено неверное значение')
+            continue
+        return user_number
+
+
+def division(a: float, b: float) -> float:
+    result = a / b
+    return result
+
+
+while True:
+    num_1 = asking('Введите первое число\n>>> ')
+    if num_1 is None:
+        break
+    num_2 = asking('Введите второе число\n>>> ', True)
+    if num_2 is None:
+        break
+    print(division(num_1, num_2), '\n')
+
