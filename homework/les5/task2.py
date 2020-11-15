6bb3f728df5393995dfd9ee32b1a4ba8f3815a44
AlinaBarinova <popowa.ai4@gmail.com>
2020-11-15 16:41:14 +0300

    les5 task 2

---
+++ homework/les5/task2.py
@@ -0,0 +1,9 @@
+cnt_lines, cnt_words = 0, []
+with open('file_task2.txt') as open_file:
+    for line in open_file:
+        cnt_lines += 1
+        cnt_words.append(line.count(' ') + 1)
+
+print(f"Количество строк = {cnt_lines}")
+for num, cnt in enumerate(cnt_words):
+    print(f"Строка: {num+1:2} Слов: {cnt:2}")
